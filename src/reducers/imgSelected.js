b4eaa4a2cd41183c3c7e7c1b2759e61bff0ea0cb
alejandro garcia robles <alejandro.garcia@bairesdev.com>
2015-11-27 19:16:03 +0100

    Screenshot added

--- src/reducers/imgSelected.js
+++ src/reducers/imgSelected.js
@@ -1,7 +1,8 @@
 import { ASSIGN } from '../actions/img'
 
 export default function selected(state="key1", action) {
-  console.log("mutation here!" + action);
+  console.log("mutation invoked!");
+  console.log(action);
 
   if(action.type===ASSIGN) {
     state = action.newVal;
